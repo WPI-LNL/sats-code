8f3c6c24e7262b72de8dc5b7a56e060fd5875de1
Mitchell Jacobs <mrjacobs@wpi.edu>
2022-10-23 17:23:46 -0400

    testing1

---
+++ 1wire/src/1wire.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "1wire.h"
+
+#define TRUE 1
+#define FALSE 0
+
+int main() {
+    printf("Hello world\n");
+
+    char busSearch[] = "/sys/bus/w1/devices/w1_bus_master1/w1_master_search";
+    char busRemove[] = "/sys/bus/w1/devices/w1_bus_master1/w1_master_remove";
+    char busSlaves[] = "/sys/bus/w1/devices/w1_bus_master1/w1_master_slaves";
+
+    FILE* fsearch;
+    FILE* fremove;
+    FILE* fslaves;
+
+    //while(FALSE) {
+        fslaves = fopen(busSlaves, "r");
+        char* line;
+        size_t len;
+
+        while(getline(&line, &len, fslaves) != -1 ){
+            fremove = fopen(busRemove, "w");
+            fprintf(fremove, "%s", line);
+            fclose(fremove);
+        }
+
+    //}
+
+    printf("%s + \n", busSearch);
+};
+
+
+
