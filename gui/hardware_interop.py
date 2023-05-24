1dd61962729601d8cda4c53d3b56edc2e99feaf0
Benjamin A <yellowjaguar5@gmail.com>
2023-05-24 15:59:00 -0400

    Update hardware_interop.py

--- gui/hardware_interop.py
+++ gui/hardware_interop.py
@@ -1,8 +1,12 @@
 import onewire_interface
 
 class Hardware_Interop:
-    def change_callback(changes: tuple):
-        pass
+    def __init__(self, callback_UpdateSlot: function, callback_HardwareStateChange: function):
+        onewire_interface.registerCallback_UpdateSlot(callback_UpdateSlot)
+        onewire_interface.registerCallback_HardwareStateChange(callback_HardwareStateChange)
 
-    def __init__(self):
-        onewire_interface.registerCallback(Hardware_Interop.change_callback)
+    def setStatusColor(self, position: int, color: tuple[int]):
+        onewire_interface.setStatusColor(position, color)
+    
+    def setStatusColors(self, colors: list[tuple[int]]):
+        onewire_interface.setStatusColors(colors)
